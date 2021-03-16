6496b30d8a49fcab6dbdbf4c12a09dcaa9137fa4
Maxim Maslin <zapzarap@yandex.ru>
2021-03-16 18:33:41 +0300

    fix

--- go/1_go_basics/chapter_1/main.go
+++ go/1_go_basics/chapter_1/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	// "github.com/mxmaslin/skillfactory/go/1_go_basics/chapter_1/maths"
-	a "chapter_1/maths/addition"
-	s "chapter_1/maths/subtraction"
+	m "chapter_1/maths"
 )
 
 func main() {
-	fmt.Println(a.AdditionResult)
-	fmt.Println(s.SubtractionResult)
+	fmt.Println(m.AdditionResult)
+	fmt.Println(m.SubtractionResult)
 
 }
