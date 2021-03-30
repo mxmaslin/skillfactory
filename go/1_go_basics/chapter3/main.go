57c52e35a84572c7ea407e66f458497563d6c670
Maxim Maslin <zapzarap@yandex.ru>
2021-03-30 12:31:31 +0300

    import: calculate -> calc

--- go/1_go_basics/chapter3/main.go
+++ go/1_go_basics/chapter3/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	calculate "github.com/mxmaslin/skillfactory/go/1_go_basics/chapter3/calc"
+	calc "github.com/mxmaslin/skillfactory/go/1_go_basics/chapter3/calc"
 )
 
 func main() {
-	c := calculate.SetCalculator(1.0, 2.0, "+")
+	c := calc.SetCalculator(1.0, 2.0, "+")
 	fmt.Println("%v", c)
 }
