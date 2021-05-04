f9ca59748a517da814d90d7d367517e18fa4ebdc
Maxim Maslin <zapzarap@yandex.ru>
2021-05-04 21:57:46 +0300

    implement insertion sort

---
+++ go/2_algorithms/sorting_algorithms/insertion_sort.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+	// int_helper "github.com/mxmaslin/skillfactory/go/2_algorithms/sorting_algorithms/int_helper"
+)
+
+
+func init() {
+	rand.Seed(time.Now().UnixNano()) // необходимо для того, чтобы рандом был похож на рандомный
+}
+
+func main() {
+	arr := make([]int, 10)
+	for i := range arr {
+		arr[i] = rand.Intn(20) - 10 // ограничиваем случайно значение от [-100;100]
+	}
+
+	fmt.Println(arr)
+	insertionSort(arr)
+	fmt.Println(arr)
+}
+
+
+func insertionSort(arr []int) {
+	j := 0
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		j = i - 1
+		for j >= 0 && arr[j] > key {
+			arr[j + 1] = arr[j]
+			j = j - 1
+		}
+		arr[j + 1] = key
+	}
+}
